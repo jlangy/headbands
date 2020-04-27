8b79a997ef1d4640dfd448f3951ed6a55f4733fe
Jon Langlois <jonathan-langlois@live.ca>
2020-04-27 10:31:36 -0700

    rooms setup on front end

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const socket = require('socket.io');
 
+let id = 1;
+
 const app = express();
 
 const server = app.listen(3000, () => console.log('listening on port 3000'));
@@ -11,4 +13,13 @@ const io = socket(server);
 
 io.on('connection', function(socket){
   console.log('connected!');
+
+  socket.on('join room', id => {
+    if(io.sockets.adapter.rooms[id]){
+      socket.join(id);
+    }
+    else {
+      console.log('room DNE')
+    }
+  })
 })
--- public/chat.js
+++ public/chat.js
@@ -1,2 +1,32 @@
 const socket = io.connect("http://localhost:3000")
 
+const localVideo = document.getElementById('local')
+const remoteVideo = document.getElementById('remote')
+const roomId = document.getElementById('roomId')
+const makeRoomBtn = document.getElementById('makeRoom')
+const joinRoomBtn = document.getElementById('joinRoom')
+const joinRoomInput = document.getElementById('roomIdInput')
+
+makeRoomBtn.addEventListener('click', makeRoom)
+joinRoomBtn.addEventListener('click', joinRoom)
+
+function makeRoom(){
+  navigator.mediaDevices.getUserMedia({
+    audio: false,
+    video: true
+  })
+  .then(feedLocalStream)
+  .catch(function(e) {
+    alert('getUserMedia() error: ' + e.name);
+  });
+}
+
+function joinRoom(){
+  const roomId = joinRoomInput.value;
+  socket.emit('join room', roomId)
+}
+
+function feedLocalStream(stream){
+  localVideo.srcObject = stream;
+  roomId.innerText = `Connected to room ${socket.id}`
+}
