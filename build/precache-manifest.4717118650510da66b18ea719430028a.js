f6cf0c2e05e17107ee1888cd59a3d3864335fbe8
Jon Langlois <jonathan-langlois@live.ca>
2020-04-28 18:29:40 -0700

    add react for front-end

---
+++ build/precache-manifest.4717118650510da66b18ea719430028a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "424b459d3b6003102ace766f34e60454",
+    "url": "/index.html"
+  },
+  {
+    "revision": "caa07f62b62f3c1b11a5",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "d5856768ce12ef3504aa",
+    "url": "/static/js/2.1ee66824.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1ee66824.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "caa07f62b62f3c1b11a5",
+    "url": "/static/js/main.62419293.chunk.js"
+  },
+  {
+    "revision": "03ef42e68e31813f05b2",
+    "url": "/static/js/runtime-main.99b9cc22.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const socket = require('socket.io');
+const path = require('path');
+const port = process.env.PORT || 3000;
 
 let id = 1;
 let rooms = [];
@@ -8,9 +10,16 @@ const app = express();
 
 const server = app.listen(3000, '0.0.0.0', () => console.log('listening on port 3000'));
 
-app.use(express.static('public'));
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 const io = socket(server);
+
+
 
 io.on('connection', function(socket){
   console.log('connected!');
@@ -23,7 +32,6 @@ io.on('connection', function(socket){
     if(io.sockets.adapter.rooms[id]){
       socket.to(id).emit('message', {type: 'joinRequest', roomId: id})
     } else {
-      console.log('hih hi in here')
       socket.emit('message', {type:'room does not exist'})
     }
   });
