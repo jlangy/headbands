60e40ac5e08a0dc3a84540d3b87b55478e32a1b6
Jon Langlois <jonathan-langlois@live.ca>
2020-05-08 11:52:29 -0700

    turn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const socket = require('socket.io');
 const path = require('path')
-const port = 3001;
+const port = process.env.PORT || 5000;
 
 const app = express();
 // const server = app.listen(port, '0.0.0.0', () => console.log(`listening on port ${port}`));
