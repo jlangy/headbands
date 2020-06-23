d0a8f80cefb62acebd04e766e05263d02e80b221
Bryce McMath <bryce.j.mcmath@gmail.com>
2020-06-23 14:32:35 -0700

    Update SAlert and change logo

--- src/styled_components/layout/SAlert.js
+++ src/styled_components/layout/SAlert.js
@@ -1,15 +1,16 @@
 import styled from 'styled-components';
 
 const SAlert = styled.p`
-	font-family: 'Cabin', Helvetica, Arial, sans-serif;
+	font-family: 'Permanent Marker', cursive;
 	display: flex;
 	justify-content: center;
 	align-items: center;
 	padding: 0.75rem 1.5rem;
 	text-align: center;
-	background-color: #3ca7d280;
-	color: white;
-	text-transform: uppercase;
+	background-color: #fdfea1;
+	color: #555;
+	text-transform: capitalize;
+	font-size: 1.5rem;
 `;
 
 export default SAlert;
