f0949fcf5ec53f842a4ea4de13b88af5a15a73af
Jon Langlois <jonathan-langlois@live.ca>
2020-06-23 09:39:02 -0700

    media query for how to play

--- src/styled_components/layout/SInfoImage.js
+++ src/styled_components/layout/SInfoImage.js
@@ -6,7 +6,12 @@ const SInfoImage = styled.img`
   width: 65%;
   margin-left: 20px;
   border: 1px solid #3ca7d2;
-  min-width: 200px;
+
+  @media (max-width: 480px) {
+    float: unset;
+    width: 100%;
+    margin: 0;
+	}
 `;
 
 export default SInfoImage;
